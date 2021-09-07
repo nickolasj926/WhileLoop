de7e78c1432944f10991417abe1db8db6fb31401
nickolasj926 <nickolasj926@gmail.com>
2021-09-07 17:43:01 -0500

    Initial commit: while loop practice.

---
+++ src/whileLoop.java
@@ -0,0 +1,9 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int years = 14;
+        while (years < 23) {
+            System.out.println(years);
+            years++;
+        }
+    }
+}
